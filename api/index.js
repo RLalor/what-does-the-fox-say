94bcc9c4107fefaa8d95645472f9629426843434
rory lalor <rorylalor83@gmail.com>
2024-07-23 18:06:41 +0700

    remove vercel specific code

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
 "use strict";
 import express from "express";
 import axios from "axios";
+import path from "path";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = process.env.PORT || 3000; // PORT FOR VERCEL DEPLOYMENT
-// const port = 3000; LOCAL USE PORT
+const port = process.env.PORT || 3000; // PORT FOR RENDER DEPLOYMENT
 
-app.use(express.static("public"));
+// const port = 3000; // LOCAL USE PORT
+
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "/views"));
+// app.set("../views"));
+
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
